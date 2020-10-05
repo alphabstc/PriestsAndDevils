2c732d768f8a8ba03da0be863c49edb1c04a3f26
alphabstc <51530816+alphabstc@users.noreply.github.com>
2020-10-05 23:36:54 +0800

    upd

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -9,24 +9,20 @@ public class Click : MonoBehaviour
 	IUserAction action;
 	RoleModel role = null;
 	BoatModel boat = null;
-	public void SetRole(RoleModel role)
-	{
+	public void SetRole(RoleModel role) {//设置当前选中的角色
 		this.role = role;
 	}
-	public void SetBoat(BoatModel boat)
-	{
+	public void SetBoat(BoatModel boat) {//设置当前选中的船
 		this.boat = boat;
 	}
-	void Start()
-	{
-		action = GameDirector.GetInstance().CurrentScenceController as IUserAction;
+	void Start() {
+		action = GameDirector.GetInstance().CurrentScenceController as IUserAction;//通过导演获取当前场景的主控制器
 	}
-	void OnMouseDown()
-	{
-		if (boat == null && role == null) return;
-		if (boat != null)
-			action.MoveBoat();
-		else if(role != null)
-			action.MoveRole(role);
+	void OnMouseDown() {//鼠标按下
+		if (boat == null && role == null) return;//没有选中船或角色
+		if (boat != null)//选中了船
+			action.MoveBoat();//移动船
+		else if(role != null)//选中了角色
+			action.MoveRole(role);//移动角色
 	}
 }
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using interfaceController;
 
-public class GameDirector : System.Object {
+public class GameDirector : System.Object {//导演类
 	private static GameDirector _instance;
 	public ISceneController CurrentScenceController { get; set; }
-	public static GameDirector GetInstance() {
-		if (_instance == null) {
+	public static GameDirector GetInstance() {//单体设计模式
+		if (_instance == null) {//创建单体
 			_instance = new GameDirector();
 		}
 		return _instance;
