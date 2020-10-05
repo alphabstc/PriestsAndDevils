571f63a92e196606da81eea17c4704f86753ad0a
alphabstc <51530816+alphabstc@users.noreply.github.com>
2020-10-05 23:18:54 +0800

    first commit

---
+++ Assets/Scripts/UserGUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using interfaceController;
+
+public class UserGUI : MonoBehaviour {
+
+	private IUserAction action;
+	public int sign = 0;
+
+	bool isShow = false;
+	void Start()
+	{
+		action = GameDirector.GetInstance().CurrentScenceController as IUserAction;
+	}
+	void OnGUI()
+	{
+		GUIStyle text_style;
+		GUIStyle button_style;
+		text_style = new GUIStyle()
+		{
+			fontSize = 30
+		};
+		button_style = new GUIStyle("button")
+		{
+			fontSize = 15
+		};
+		if (GUI.Button(new Rect(10, 10, 60, 30), "规则", button_style))
+		{
+			isShow = !isShow;
+		}
+		if(isShow)
+		{
+			GUI.Label(new Rect(Screen.width / 2 - 114, 10, 200, 50), "需要让三个牧师和三个魔鬼都过河");
+			GUI.Label(new Rect(Screen.width / 2 - 120, 30, 250, 50), "每一边恶魔数量都不能多于牧师数量");
+			GUI.Label(new Rect(Screen.width / 2 - 85, 50, 250, 50), "船同时最多只能载两个人");
+			GUI.Label(new Rect(Screen.width / 2 - 85, 70, 250, 50), "点击牧师、魔鬼、船操作");
+		}
+		if (sign == 1)
+		{
+			GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height / 2-120, 100, 50), "你输了！", text_style);
+			if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2, 100, 50), "重新开始", button_style))
+			{
+				action.Restart();
+				sign = 0;
+			}
+		}
+		else if (sign == 2)
+		{
+			GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 120, 100, 50), "你赢了！", text_style);
+			if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2, 100, 50), "重新开始", button_style))
+			{
+				action.Restart();
+				sign = 0;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/interfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using modelController;
+
+namespace interfaceController{
+	public interface ISceneController                      
+	{
+		void LoadResources();
+	}
+	public interface IUserAction                           
+	{
+		void MoveBoat();                                   
+		void Restart();                                    
+		void MoveRole(RoleModel role);                     
+		int Check();                                       
+	}
+}
